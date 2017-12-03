c051b3ca4a52effe7b816e1218916ba0a423c4ab
juan rojas <15171439@itculiacan.edu.mx>
2017-12-02 23:13:07 -0700

    Creacion del Menu

--- Restaurant/Restaurant/App_Start/RouteConfig.cs
+++ Restaurant/Restaurant/App_Start/RouteConfig.cs
@@ -18,6 +18,43 @@ namespace Restaurant
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "SP_Insertar_Producto",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "SP_Insertar", action = "SP_Insertar_Producto", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "SP_Cancelar_Pedido",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "SP_Cancelar_Pedido", action = "SP_Cancelar_Pedido", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "SP_Modificar",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "SP_Modificar", action = "SP_Modificar_Ingrediente", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "SP_OrdenxEstado",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "SP_OrdenxEstado", action = "SP_Orden_Estado", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "SP_Nuevo_Pedido",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "SP_Nuevo_Pedido", action = "SP_Alta_Pedido", id = UrlParameter.Optional }
+            );
+
+
+            routes.MapRoute(
+                name: "SP_Ventas_Rango",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "SP_Ventas", action = "SP_Ventas_Rango", id = UrlParameter.Optional }
+            );
         }
     }
 }
